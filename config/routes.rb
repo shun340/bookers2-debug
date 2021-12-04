6bdc6abc3cb2afaf5d70abfaa0efe676de7b0f0c
shun340 <92897220+shun340@users.noreply.github.com>
2021-12-04 13:42:20 +0000

    [Finish]3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
-  resources :users, only: [:show,:index,:edit,:update,]
-  resources :books
-  
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :favorites, only: [:create,:destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
 end
