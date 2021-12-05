cf61a4e3bc9f804c17bc7c911160b924e62c2465
shun340 <92897220+shun340@users.noreply.github.com>
2021-12-05 17:08:35 +0000

    [Finish]4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root :to => 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
-  resources :users, only: [:show,:index,:edit,:update]
+
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
     resource :favorites, only: [:create,:destroy]
     resources :book_comments, only: [:create, :destroy]
